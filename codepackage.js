882e7ed2e18d966b4c3d99d4da95735361420724
Morten Henriksen <mh@gi2.dk>
2014-02-16 17:43:44 +0100

    break

---
+++ codepackage.js
@@ -0,0 +1,43 @@
+var exportObject = { npmDepend: [] }, Npm = {depends: exportObject.npmDepend.push }; Package = {describe: function(obj) {exportObject.describe = obj;},on_use: function(fn) {exportObject.on_use = fn;},on_test: function(fn) {exportObject.on_test = fn;}};
+Package.describe({
+  summary: 'CollectionFS, FS.File object'
+});
+
+Npm.depends({
+  mime: "1.2.11"
+});
+
+Package.on_use(function(api) {
+
+  api.use(['cfs-base-package', 'cfs-storage-adapter']);
+
+  api.use(['deps', 'underscore', 'check', 'livedata', 'mongo-livedata' ]);
+
+  api.use(['cfs-filesaver'], 'client');
+
+  api.add_files([
+    'fsFile-common.js',
+    'fsFile-data-get.js',
+    'fsFile-data-set.js',
+    'fsFile-client.js'
+  ], 'client');
+
+  api.add_files([
+    'fsFile-common.js',
+    'fsFile-data-get.js',
+    'fsFile-data-set.js',
+    'fsFile-server.js'
+  ], 'server');
+});
+
+Package.on_test(function (api) {
+  api.use('collectionFS');
+  api.use('test-helpers', 'server');
+  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
+           'random', 'deps']);
+
+  api.add_files('tests/server-tests.js', 'server');
+  api.add_files('tests/client-tests.js', 'client');
+});
+
+module.exports = exportObject;
